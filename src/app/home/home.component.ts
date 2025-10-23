ff773b643af44b9753c64c02b74b95d5f7a7e0de
nxtbrasil <contato@nxtbrasil.com>
2025-10-23 01:09:23 -0300

    ajuste menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from './menu.service';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      state('void', style({ height: '0', opacity: 0, overflow: 'hidden' })),
+      state('*', style({ height: '*', opacity: 1, overflow: 'hidden' })),
+      transition('void <=> *', animate('200ms ease-in-out')),
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   menu: any[] = [];
+  openedMenu: number | null = null;
 
   constructor(private menuService: MenuService) {}
 
@@ -16,5 +31,9 @@ export class HomeComponent implements OnInit {
       next: data => this.menu = data,
       error: err => console.error(err)
     });
+  }
+
+  toggleMenu(id: number): void {
+    this.openedMenu = this.openedMenu === id ? null : id;
   }
 }
--- src/app/shared/menu/submenu.component.ts
+++ src/app/shared/menu/submenu.component.ts
@@ -1,16 +1,33 @@
 import { Component, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-submenu',
-  template: `
-    <ul class="submenu">
-      <li *ngFor="let item of nodes">
-        <a [routerLink]="[item.endereco]" class="submenu-link">{{ item.descricao }}</a>
-      </li>
-    </ul>
-  `,
-  styleUrls: ['./submenu.component.css']
+  templateUrl: './submenu.component.html',
+  styleUrls: ['./submenu.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({
+        height: '0px',
+        opacity: 0,
+        overflow: 'hidden',
+      })),
+      state('expanded', style({
+        height: '*',
+        opacity: 1,
+        overflow: 'hidden',
+      })),
+      transition('collapsed <=> expanded', [
+        animate('250ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class SubmenuComponent {
   @Input() nodes: any[] = [];
+  expanded = false;
+
+  toggleExpand() {
+    this.expanded = !this.expanded;
+  }
 }
